b4a14e1f09c8f80de4ec0339500f86cea62b2ad8
Jean-Francois Renaud <jfrenaud@gema-solutions.com>
2022-06-12 09:45:56 -0400

    checkbox interaction test

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,5 +1,7 @@
 import Checkbox from "./Checkbox.vue";
 import { Meta, StoryFn } from "@storybook/vue3";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 export default {
   title: "Components/Checkbox",
@@ -13,8 +15,29 @@ Default.args = {
   id: "id",
 };
 
+export const ClickOnCheckbox = Template.bind({});
+ClickOnCheckbox.args = {
+  id: "id",
+};
+ClickOnCheckbox.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole("checkbox"));
+  await expect(canvas.getByRole("checkbox")).toBeChecked();
+};
+
 export const Label = Template.bind({});
 Label.args = {
   id: "id",
   label: "Label",
+};
+
+export const ClickOnLabel = Template.bind({});
+ClickOnLabel.args = {
+  id: "id",
+  label: "Label",
+};
+ClickOnLabel.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByText("Label"));
+  await expect(canvas.getByRole("checkbox")).toBeChecked();
 };
